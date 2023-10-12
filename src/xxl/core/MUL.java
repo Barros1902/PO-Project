32894f8c58e1bc83fd15f99e79ff47fcff662a7c
poicoLaptop <rodrigodecgomes@gmail.com>
2023-10-12 17:53:25 +0100

    Mul

--- src/xxl/core/MUL.java
+++ src/xxl/core/MUL.java
@@ -11,12 +11,28 @@ public class MUL extends BinaryFunction {
             int val1 = getValueArg1().EvalInt();
             int val2 = getValueArg2().EvalInt();
 
-            return val1*val2;
+            return isReference(getValueArg1())*isReference(getValueArg2());
         } catch(Exception e){
             throw new ArrayCharException();
-            //TODO: add exception can't add strings
         }
     }
+    private int isReference(Content arg){
+        if(arg.toString().contains("=")){
+            return Integer.parseInt(arg.toString().split("=")[0]);
+        } else {
+            if (arg.EvalInt()==0 && arg.evalString()==null)
+                return 1;
+            return arg.EvalInt();
+        }
+    }
+    private String Transform(Content arg){
+        if(arg.toString().contains("=")){
+            return String.valueOf(arg.toString().split("=")[1]);
+        } else {
+            return arg.toString();
+        }
+    }
+
 
     @Override
     protected Literals value() {
@@ -25,9 +41,10 @@ public class MUL extends BinaryFunction {
     @Override
     public String toString(){
         try {
-            return String.valueOf(evalInt())+"=MUL("+getValueArg1().toString()+","+getValueArg1().toString()+")";
+            return String.valueOf(evalInt())+"=MUL("+Transform(getValueArg1())+","+Transform(getValueArg2())+")";
         } catch (ArrayCharException e) {
             throw new RuntimeException(e);
         }
+
     }
 }
