2977c6ea7dd7300b45f2cadd6a7bc5bd1849af5c
Tomas Barros <ist1106588@tecnico.ulisboa.pt>
2023-10-26 16:22:52 +0100

    dodelete done

--- src/xxl/app/edit/DoDelete.java
+++ src/xxl/app/edit/DoDelete.java
@@ -2,8 +2,12 @@ package xxl.app.edit;
 
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
+import xxl.app.exception.InvalidCellRangeException;
+import xxl.core.CutBuffer;
+import xxl.core.Gama;
 import xxl.core.Spreadsheet;
 // FIXME import classes
+import xxl.core.exception.OutOfBoundsException;
 
 /**
  * Delete command.
@@ -12,11 +16,18 @@ class DoDelete extends Command<Spreadsheet> {
 
   DoDelete(Spreadsheet receiver) {
     super(Label.DELETE, receiver);
-    // FIXME add fields
+	addStringField("range", Message.address());
   }
   
   @Override
   protected final void execute() throws CommandException {
-    // FIXME implement command
+	String range = stringField("range");
+	try {
+      Gama gama = _receiver.createRange(range);
+	  gama.clear();
+    } catch (OutOfBoundsException e) {
+      throw new InvalidCellRangeException(range);
+
+  }
   }
 }
